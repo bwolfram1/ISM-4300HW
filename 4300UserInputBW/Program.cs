0cd3edb9573d6892a76073f0669afd55831124e9
Brandon <brandonmwolfram@outlook.com>
2020-01-31 01:45:52 -0500

    Second Assignment for ISM 4300

---
+++ 4300UserInputBW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4300UserInputBW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Writen by Brandon Wolfram for ISM 4300
+            string UInput;
+            Console.WriteLine("Hi, this is Brandon");
+            Console.WriteLine("Please enter something and press enter.");
+            UInput = Console.ReadLine();
+            Console.WriteLine("You entered: " + UInput);
+
+        }
+    }
+}
